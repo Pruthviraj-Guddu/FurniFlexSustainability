0f28eaafc570f14b28e28730d68d452acd8e0d49
Adesewa Adesida <adesewa.adesida@gmail.com>
2024-04-20 14:29:10 -0400

    Furni Db

--- dev/furni_db.sql
+++ dev/furni_db.sql
@@ -1,60 +1,67 @@
 
+
 --drop table in case your db already
-DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Student CASCADE CONSTRAINTS;
 -- Create table
 CREATE TABLE Student (
-    studentEmail VARCHAR(100) PRIMARY KEY,
-    name VARCHAR(100),
-    phoneNumber VARCHAR(20),
-    startYear INT,
-    schoolName VARCHAR(100),
-    address VARCHAR(255)
+                         studentEmail VARCHAR(100) PRIMARY KEY,
+                         name VARCHAR(100) NOT NULL,
+                         phoneNumber VARCHAR(20) NOT NULL,
+                         startYear INT NOT NULL,
+                         schoolName VARCHAR(100) NOT NULL,
+                         address VARCHAR(255) NOT NULL
 );
 
 --drop table in case your db already
-DROP TABLE IF EXISTS Accounts;
+DROP TABLE IF EXISTS Accounts CASCADE CONSTRAINTS;
 -- Create table
 CREATE TABLE Accounts (
-    accountID INT PRIMARY KEY,
-    studentEmail VARCHAR(100) UNIQUE,
-    username VARCHAR(50),
-    password VARCHAR(50),
-    graduationYear INT,
-    startYear INT,
-    expirationDate DATE,
-    paymentCardInformation VARCHAR(100),
-    schoolName VARCHAR(100)
+                          accountID INT PRIMARY KEY,
+                          studentEmail VARCHAR(100) UNIQUE,
+                          username VARCHAR(50) NOT NULL ,
+                          password VARCHAR(50) NOT NULL,
+                          graduationYear INT NOT NULL,
+                          startYear INT NOT NULL,
+                          expirationDate DATE NOT NULL ,
+                          paymentCardInformation VARCHAR(100),
+                          FOREIGN KEY (studentEmail) REFERENCES Student(studentEmail)
 );
 
 --drop table in case your db already
-DROP TABLE IF EXISTS Listings;
+DROP TABLE IF EXISTS Listings CASCADE CONSTRAINTS;
 -- Create table
 CREATE TABLE Listings (
-    itemID INT PRIMARY KEY,
-    ownerID INT REFERENCES Accounts(accountID),
-    price INT,
-    description VARCHAR(255),
-    sellByDate DATE,
-    status VARCHAR(20)
+                          itemID INT PRIMARY KEY,
+                          ownerID INT NOT NULL ,
+                          price INT NOT NULL,
+                          description VARCHAR(255),
+                          sellByDate DATE,
+                          status VARCHAR(20) NOT NULL,
+                          FOREIGN KEY (ownerID) REFERENCES Accounts(accountID)
 );
 
 --drop table in case your db already
-DROP TABLE IF EXISTS Transactions;
+DROP TABLE IF EXISTS Transactions CASCADE CONSTRAINTS;
 -- Create table
 CREATE TABLE Transactions (
-    transactionID INT PRIMARY KEY,
-    buyerID INT REFERENCES Accounts(accountID),
-    sellerID INT REFERENCES Accounts(accountID),
-    itemID INT REFERENCES Listings(itemID)
+                              transactionID INT PRIMARY KEY,
+                              buyerID INT NOT NULL,
+                              sellerID INT NOT NULL,
+                              itemID INT NOT NULL ,
+                              FOREIGN KEY (buyerID) REFERENCES Accounts(accountID),
+                              FOREIGN KEY (sellerID) REFERENCES Accounts(accountID),
+                              FOREIGN KEY (itemID) REFERENCES Listings(itemID)
 );
 
 --drop table in case your db already
-DROP TABLE IF EXISTS Messages;
+DROP TABLE IF EXISTS Messages CASCADE CONSTRAINTS;
 -- Create table
 CREATE TABLE Messages (
-    messageID INT PRIMARY KEY,
-    senderID INT REFERENCES Accounts(accountID),
-    receiverID INT REFERENCES Accounts(accountID),
-    encryptedContent VARCHAR(4000)
+                          messageID INT PRIMARY KEY,
+                          senderID INT NOT NULL,
+                          receiverID INT NOT NULL ,
+                          encryptedContent VARCHAR(4000),
+                          FOREIGN KEY (senderID) REFERENCES Accounts(accountID) ,
+                          FOREIGN KEY (receiverID) REFERENCES Accounts(accountID)
 );
 
